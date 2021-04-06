fdf8c77aa0a8302438e3b4119ee576572baa8bcb
Harshna Goratela <harshna.goratela@gmail.com>
2021-04-06 19:07:58 +0530

    06-apr changes

---
+++ src/components/OpenSea/RecentSales.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Heading, Button, Text, Box, Flex, Card, Link, Loader, Image } from 'rimble-ui';
+
+
+const RecentSales = (props) => {
+    const [loaded, setLoaded] = React.useState(false);
+    const [data, setData] = React.useState([]);
+    const limitSteps = 20
+    const [currentLimit, setCurrentLimit] = React.useState(limitSteps);
+
+    const fetchData = (account) => {
+        const fetch = require('node-fetch');
+        const url = 'https://api.opensea.io/api/v1/events';
+        let qs = '?event_type=successful&only_opensea=false&offset=0&limit=' + currentLimit
+        if (account) qs += "&account_address=" + account;
+        const options = { method: 'GET' };
+        // console.log("****** url", currentLimit, url + qs)
+        fetch(url + qs, options)
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                setLoaded(true)
+                setData(json.asset_events)
+            })
+            .catch(err => console.error('error:' + err));
+    }
+
+    const loadMore = () => {
+        setLoaded(false)
+        setCurrentLimit(currentLimit + limitSteps)
+    }
+
+    React.useEffect(() => {
+        fetchData('0x694e64d4ad77e0c234b7b1c55ac40302ad86ce3f');
+    }, [currentLimit]);
+
+    return (
+        <Box width={1} my={1} p={2} border='1px solid'>
+            <Heading>
+                RECENT SALES
+                {!loaded && <Loader mx={2} style={{ display: "inline-flex" }} />}
+            </Heading>
+            {data && data.map((item, index) => {
+                const imageURL = item.asset && (item.asset.image_thumbnail_url || item.asset.image_preview_url || item.asset.image_url)
+                const name = item.asset && (item.asset.name || 'No Title')
+                const assetLink = item.asset && (item.asset.permalink || '#')
+                const collectionName = item.asset && item.asset.collection && item.asset.collection.name
+                const collectionLink = item.asset && item.asset.collection && item.asset.collection.slug
+                const price = (item.total_price || 0) / 1000000000000000000;
+                const sellername = item.seller && item.seller.user && item.seller.user.username
+                return (
+                    <Card px={3} key={index}>
+                        <Flex>
+                            <Box>
+                                <Image src={imageURL} alt={name} width={80} borderRadius={8} />
+                            </Box>
+                            <Box px={3}>
+                                <Link href={assetLink} target="_blank"><Heading as={'h4'}>{name}</Heading></Link>
+                                {collectionName &&
+                                    <Link href={`http://opensea.io/collection/${collectionLink}`} target="_blank"><Text>{collectionName}</Text></Link>
+                                }
+                                <Text>Price: {price} ETH</Text>
+                                {sellername &&
+                                    <Text>Sold by: {sellername}</Text>
+                                }
+                            </Box>
+                        </Flex>
+                    </Card>
+                )
+            })}
+
+            {data && data.length > 0 &&
+                <Flex justifyContent='flex-end' my={3}>
+                    {!loaded && <Loader size='30px' marginRight={3} style={{ display: "inline-flex" }} />}
+                    <Button size="small" onClick={loadMore}>Load More</Button>
+                </Flex>
+            }
+        </Box>
+    )
+}
+
+export default RecentSales;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 // Frameworks
 import React from 'react';
-import { Heading, Button, Text, Box, Link, Loader, Image, Table } from 'rimble-ui';
+import { navigate } from 'gatsby';
+import { Heading, Button, Text, Box, Flex, Card, Modal, Link, Loader, Image, Table } from 'rimble-ui';
 import styled from 'styled-components';
+import _ from 'lodash'
 
 // Layout Components
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import RecentSales from '../components/OpenSea/RecentSales'
+import RecentListings from '../components/OpenSea/RecentListings'
+
+import Wallet from '../app/wallets';
+import { RootStoreContext } from '../app/stores/root.store';
 
 const TableCellHeading = styled.th`
   padding: 1rem !important;
@@ -16,30 +23,125 @@ const TableCell = styled.td`
 
 const IndexPage = () => {
 
-    const [loaded, setLoaded] = React.useState(false);
-    const [collections, setCollections] = React.useState([]);
+    const rootStore = React.useContext(RootStoreContext);
+    const isLoggedIn = rootStore.walletStore.hasAccount;
 
-    React.useEffect(() => {
-        //get collections
-        const fetch = require('node-fetch');
-        const url = 'https://api.opensea.io/api/v1/collections';
-        const options = { method: 'GET', qs: { offset: '0', limit: '300'} };
-        fetch(url, options)
-            .then(res => res.json())
-            .then(json => {
-                console.log(json)
-                setLoaded(true)
-                setCollections(json.collections)
-            })
-            .catch(err => console.error('error:' + err));
-    }, []);
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const closeModal = e => {
+        e.preventDefault();
+        setIsOpen(false);
+    };
+
+    const openModal = e => {
+        e.preventDefault();
+        setIsOpen(true);
+    };
+
+    const wallet = Wallet.instance();
+    const _walletConnect = (walletType) => async () => {
+        try {
+            // console.log("***** login clicked")            
+            await wallet.init(walletType);
+            await wallet.connect();
+            setIsOpen(false);
+            //navigate(`/`);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+    const _logout = async () => {
+        await wallet.disconnect();
+        navigate(`/`);
+    };
+
+    const walletButtons = _.map(Wallet.typeMap(), (walletData, walletType) => {
+        const disabled = !Wallet.isEnabled(walletType);
+        return (
+            <Box key={walletType} m={2}>
+                <Button size="small" onClick={_walletConnect(walletType)} disabled={disabled}>{walletData.name}</Button>
+            </Box>
+        );
+    });
 
     return (
         <Layout>
             <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
             <Box mt={4}>
                 <Text.p style={{ float: "right" }}>
-                    <Button size="small" as="a" href="/app/">Ethereum Dapp</Button>
+                    {!isLoggedIn &&
+                        <Button size="small" onClick={openModal}>Login</Button>
+                    }
+                    {isLoggedIn &&
+                        <Button size="small" onClick={_logout}>Logout</Button>
+                    }
+                </Text.p>
+            </Box>
+            <Heading as={"h2"}>
+                Manage My Assets
+            </Heading>
+            <Flex flexWrap='wrap'>
+                <Card>
+                    <Link href="#" target="_blank"><Heading as={'h4'}>Title</Heading></Link>
+                    <Image src="https://via.placeholder.com/150" borderRadius={8} />
+                    <Text>Description</Text>
+                </Card>
+                <Card>
+                    <Link href="#" target="_blank"><Heading as={'h4'}>Title</Heading></Link>
+                    <Image src="https://via.placeholder.com/150" borderRadius={8} />
+                    <Text>Description</Text>
+                </Card>
+                <Card>
+                    <Link href="#" target="_blank"><Heading as={'h4'}>Title</Heading></Link>
+                    <Image src="https://via.placeholder.com/150" borderRadius={8} />
+                    <Text>Description</Text>
+                </Card>
+            </Flex>
+            <Flex mt={3}>
+                <RecentSales />
+                <RecentListings />
+            </Flex>
+
+            <Modal isOpen={isOpen}>
+                <Card width={"420px"} p={0}>
+                    <Button.Text
+                        icononly
+                        icon={"Close"}
+                        color={"moon-gray"}
+                        position={"absolute"}
+                        top={0}
+                        right={0}
+                        mt={3}
+                        mr={3}
+                        onClick={closeModal}
+                    />
+
+                    <Box p={4} mb={3}>
+                        <Heading.h3>Choose Login Option</Heading.h3>
+                        <Flex flexWrap='wrap'>{walletButtons}</Flex>
+                    </Box>
+
+                    <Flex
+                        px={4}
+                        py={3}
+                        borderTop={1}
+                        borderColor={"#E8E8E8"}
+                        justifyContent={"flex-end"}
+                    >
+                        <Button.Outline size='small' onClick={closeModal}>Close</Button.Outline>
+                    </Flex>
+                </Card>
+            </Modal>
+            {/* <Box mt={4}>
+                <Text.p style={{ float: "right" }}>
+                    {walletButtons}
+                    {!isLoggedIn &&
+                        <Button size="small" as="a" href="/app/">Login</Button>
+                    }
+                    {isLoggedIn &&
+                        <Button size="small" onClick={_logout}>Logout</Button>
+                    }
                 </Text.p>
             </Box>
             <Heading as={"h2"}>
@@ -60,7 +162,7 @@ const IndexPage = () => {
                     {!loaded && <tr><TableCell><Loader style={{ display: "inline-flex" }} />Loading Collections...</TableCell></tr>}
                     {collections && collections.map((collection, index) => (
                         <tr key={index}>
-                            <TableCell>{index+1}</TableCell>
+                            <TableCell>{index + 1}</TableCell>
                             <TableCell><Link href={`/collection/${collection.slug}`}>{collection.name}</Link></TableCell>
                             <TableCell><Image src={collection.image_url} alt={collection.name} borderRadius={8} width={80} /></TableCell>
                             <TableCell>{collection.stats.total_volume}</TableCell>
@@ -69,7 +171,7 @@ const IndexPage = () => {
                         </tr>
                     ))}
                 </tbody>
-            </Table>
+            </Table> */}
         </Layout>
     );
 };
